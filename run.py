3e724fcc2e24ef2a56ce4777a135440cecd538dd
KlaudiaBC <klablaco@gmail.com>
2022-04-11 00:17:28 +0000

    Change the data structure and build the function to print random response

--- run.py
+++ run.py
@@ -159,8 +159,8 @@ def main():
 # Functions connected with chatbot converations
 # Initialize Chatbot Training
 words = []
-classes = []
-documents = []
+tags = []
+patterns = []
 responses = []
 ignore_words = ['?', '!']
 
@@ -168,23 +168,14 @@ data_file = open('intents.json').read()
 # Convert the JSON data into Python object
 intents = json.loads(data_file)
 
-
-# Text preprocessing
-# Bag of words - words included in
-# tags and patterns, which will be a base for
-# search for the right answer
 for intent in intents['Intents']:
-    for pattern in intent['patterns']:
-        # take each word and tokenize it
-        w = nltk.word_tokenize(pattern)
-        words.extend(w)
-        # adding documents (patterns)
-        documents.append((w, intent['tag']))
-        # adding classes (tags) to the class list
-        if intent['tag'] not in classes:
-            classes.append(intent['tag'])
-    for response in intent['responses']:
-        responses.append(intent['responses'])
+    if intent['patterns'] not in patterns:
+        patterns.append((intent['patterns'], intent['tag']))
+    if intent['responses'] not in responses:
+        responses.append((intent['responses'], intent['tag']))
+
+print("patterns: ", patterns)
+print("responses: ", responses)
 
 # Lemmatization -  create base word,
 # in attempt to represent related words,
@@ -192,23 +183,6 @@ for intent in intents['Intents']:
 # root words to remove redundancy
 words = [lemmatizer.lemmatize(w) for w in words if w not in ignore_words]
 words = sorted(list(set(words)))
-
-# Create a sorted list of tags
-classes = sorted(list(set(classes)))
-
-print(len(documents), "documents", documents)
-print(len(classes), "classes", classes)
-print(len(words), "unique lemmatized words", words)
-
-# taking each response from each intent and connect it with a tag,
-# building a list of responses
-# for intent in intents['Intents']:
-#     for response in intent['responses']:
-#         # adding documents (patterns)
-#         classes.append(intent['responses'])
-
-# print(responses)
-
 
 message = input()
 
@@ -222,33 +196,11 @@ def process_answer():
         
         print(msg_lem)
 
-
-
-    # check = any(item in msg_lem for item in words)
-    # if check == True:
-
-    if msg_lem in words:
-        for word in words:
-            if word == msg_lem:
-                for document in documents:
-                    if word in document:
-                        print("doc:", document[1])
-    else:
-        print("none")
-
-
+    for pattern in patterns:
+        if msg_lem in pattern[0]:
+            msg_tag = pattern[1]
+            for response in responses:
+                if msg_tag == response[1]:
+                    print(random.choice(response[0]))
 
 process_answer()
-
-# res = [i[1] for i in documents]
-# print(res)
-
-
-
-# def display_question(question):
-#     """
-#     Iterate over key/value pairs in dict and print them
-#     """
-#     for key, value in question_dict.items():
-#         if key == question:
-#             print(value)
